13a3c26bb9cc85546f914e3c9a7cda2359cb6a6f
tikrack <tikrackcode@gmail.com>
2025-02-28 12:45:30 +0330

    feat(container): handle container component

--- app/components/shared/Container.tsx
+++ app/components/shared/Container.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Container: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <>{children}</>;
+const Container: React.FC<{
+  children?: React.ReactNode;
+  className?: React.ClassAttributes<string>;
+}> = ({ children, className }) => {
+  return (
+    <>
+      <div className={"w-[calc(100%-200px)] mx-auto"}>{children}</div>
+    </>
+  );
 };
 
 export default Container;
