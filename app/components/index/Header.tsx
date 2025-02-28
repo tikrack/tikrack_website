75bd90e66670a6f4f3eab4e912af40aede9f5b37
tikrack <tikrackcode@gmail.com>
2025-02-28 13:57:18 +0330

    feat(nav): add nav component

--- app/components/index/Header.tsx
+++ app/components/index/Header.tsx
@@ -1,12 +1,14 @@
 import Logo from "~/components/shared/Logo";
+import Nav from "~/components/index/Nav";
 
 const Header = () => {
   return (
     <>
       <header className={"w-full flex justify-between items-center"}>
         {/*Right*/}
-        <div className={"flex justify-start items-center basis-1/2"}>
+        <div className={"flex justify-start items-center basis-1/2 gap-[30px]"}>
           <Logo className={"!w-[50px]"} />
+          <Nav />
         </div>
         {/*Left*/}
         <div className={"flex justify-end items-center basis-1/2"}>
---
+++ app/components/index/Nav.tsx
@@ -0,0 +1,5 @@
+const Nav = () => {
+  return <>hi</>;
+};
+
+export default Nav;
