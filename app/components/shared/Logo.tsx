78df75887a0ef1fc25228b898f20d905ed1caa2b
tikrack <tikrackcode@gmail.com>
2025-02-28 13:08:17 +0330

    feat(link): add link to logo

--- app/components/shared/Logo.tsx
+++ app/components/shared/Logo.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Logo: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <>
-      <img
-        src="/images/logo.svg"
-        alt="tikrack logo"
-        loading={"lazy"}
-        className={`w-[100px] ${className}`}
-      />
+      <Link to={"/"}>
+        <img
+          src="/images/logo.svg"
+          alt="tikrack logo"
+          loading={"lazy"}
+          className={`w-[100px] ${className}`}
+        />
+      </Link>
     </>
   );
 };
