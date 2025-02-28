10fb1b667fe9347b4b650e187ae1d2b167b411be
tikrack <tikrackcode@gmail.com>
2025-02-28 16:23:22 +0330

    feat(set): set default time

--- app/components/shared/Time.tsx
+++ app/components/shared/Time.tsx
@@ -1,19 +1,39 @@
 import { useEffect, useState } from "react";
 
 const Time = () => {
-  const [time, setTime] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [ampm, setAmPm] = useState("AM");
 
   useEffect(() => {
-    fetch("https://api.keybit.ir/time/").then((r) => {
-      // @ts-ignore
-      return setTime(r);
-    });
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
 
-    if (time) {
-      console.log("ok");
+    if (hours > 12) {
+      setHour(hours - 12);
+      setAmPm("PM");
+    } else {
+      setHour(hours);
+      setAmPm("AM");
     }
-  }, [time]);
-  return <></>;
+    setMinute(minutes);
+    setSeconds(seconds);
+  }, []);
+
+  return (
+    <>
+      <h1>{hour}</h1>
+      <span>-</span>
+      <h1>{minute}</h1>
+      <span>-</span>
+      <h1>{seconds}</h1>
+      <span>-</span>
+      <h1>{ampm}</h1>
+    </>
+  );
 };
 
 export default Time;
