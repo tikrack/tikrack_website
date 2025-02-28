09f4f8071c5dc34c68f259edbb98a30c382d2cc3
tikrack <tikrackcode@gmail.com>
2025-02-28 13:54:05 +0330

    feat(header): handle header sections

--- app/components/index/Header.tsx
+++ app/components/index/Header.tsx
@@ -3,7 +3,16 @@ import Logo from "~/components/shared/Logo";
 const Header = () => {
   return (
     <>
-      <Logo className={"!w-[50px]"} />
+      <header className={"w-full flex justify-between items-center"}>
+        {/*Right*/}
+        <div className={"flex justify-start items-center basis-1/2"}>
+          <Logo className={"!w-[50px]"} />
+        </div>
+        {/*Left*/}
+        <div className={"flex justify-end items-center basis-1/2"}>
+          <Logo className={"!w-[50px]"} />
+        </div>
+      </header>
     </>
   );
 };
