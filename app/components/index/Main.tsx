fc8d94c64ead24d37d53cec15e8990d16b5e65cd
tikrack <tikrackcode@gmail.com>
2025-03-01 18:52:58 +0330

    feat(handle): handle right text

--- app/components/index/Main.tsx
+++ app/components/index/Main.tsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <>
-      <main className={"pt-[130px] w-full h-full flex justify-between"}>
+      <main className={"pt-[100px] w-full h-full flex justify-between"}>
         <div className="basis-1/2">
           <div className={"w-[50px] h-[50px] flex justify-center items-center"}>
             <img
@@ -15,13 +15,33 @@ const Main = () => {
               "font-semibold leading-[65px] text-[50px] tracking-[0] mt-[20px]"
             }
           >
-            Hello! I'm MohamadReza
+            Hello! I'm
+            <br />
+            MohamadReza
             <span className={"block text-gray-300 text-[40px] font-bold"}>
               TikRack
             </span>
           </h1>
         </div>
-        <div className="basis-1/2"></div>
+        <div className="basis-1/2">
+          <h1
+            className={"font-semibold leading-[60px] text-[40px] tracking-[0]"}
+          >
+            A Front & Back Developer
+            <br />
+            with a World of Ideas!
+          </h1>
+          <blockquote className={"text-gray-400 mt-3 text-[17px]"}>
+            Coming together is a beginning. Keeping together is progress. and
+            <br />
+            Working together is
+            <span className={"text-green-500"}> success</span>.
+            <span className={"text-[13px] text-black text-gray-600"}>
+              {" "}
+              Henry Ford
+            </span>
+          </blockquote>
+        </div>
       </main>
     </>
   );
