6c1540abd4a48c277dbb144a07fda929ef164bda
tikrack <tikrackcode@gmail.com>
2025-03-01 22:01:50 +0330

    feat(modal)

--- app/components/modals/Instagram.tsx
+++ app/components/modals/Instagram.tsx
@@ -12,7 +12,7 @@ const Instagram: React.FC<{
   return (
     <>
       <div
-        className={`h-[90vh] shadow-mid w-[350px] ${open ? "left-[3vw]" : "left-[-100%]"} shadow-rose-600 fixed top-1/2 -translate-y-1/2 bg-white  rounded-[20px] border-2 transition-all duration-300 border-rose-300`}
+        className={`h-[90vh] shadow-mid w-[350px] ${open ? "left-[3vw]" : "left-[-100%]"} shadow-rose-600 fixed top-1/2 -translate-y-1/2 bg-white  rounded-[20px] transition-all duration-300`}
       ></div>
     </>
   );
