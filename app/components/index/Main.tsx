1588ba4034511f90e0af72fd3dd9ef08f8838a0b
tikrack <tikrackcode@gmail.com>
2025-03-01 18:58:49 +0330

    feat(text): handle text

--- app/components/index/Main.tsx
+++ app/components/index/Main.tsx
@@ -25,13 +25,15 @@ const Main = () => {
         </div>
         <div className="basis-1/2">
           <h1
-            className={"font-semibold leading-[60px] text-[40px] tracking-[0]"}
+            className={"font-semibold leading-[60px] text-[42px] tracking-[0]"}
           >
             A Front & Back Developer
             <br />
             with a World of Ideas!
           </h1>
-          <blockquote className={"text-gray-400 mt-3 text-[17px]"}>
+          <blockquote
+            className={"leading-[28px] text-gray-400 mt-3 text-[17px]"}
+          >
             Coming together is a beginning. Keeping together is progress. and
             <br />
             Working together is
